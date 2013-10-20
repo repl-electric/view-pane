87bb23a1cd15f5a49e236f8231d54c9f3eee811d
Joseph Wilk <joe@josephwilk.net>
2013-10-20 17:41:34 +0200

    colour + magic keys

--- init.el
+++ init.el
@@ -5,3 +5,10 @@
 
 ;; Load bindings config
 (live-load-config-file "bindings.el")
+
+(color-theme-gandalf)
+
+(global-set-key (kbd "C-c <left>") 'windmove-left)
+(global-set-key (kbd "C-c <right>") 'windmove-right)
+(global-set-key (kbd "C-c <up>") 'windmove-up)
+(global-set-key (kbd "C-c <down>") 'windmove-down)
