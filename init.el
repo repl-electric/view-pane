6d0b0f9b6bf300d2253c3bcd160dbad5d097cc6e
Joseph Wilk <joe@josephwilk.net>
2013-10-20 17:33:44 +0200

    skeleton

---
+++ init.el
@@ -0,0 +1,7 @@
+;; User pack init file
+;;
+;; Use this file to initiate the pack configuration.
+;; See README for more information.
+
+;; Load bindings config
+(live-load-config-file "bindings.el")
