4caec0dc4ad6ab1a37f772d3e1b3c6352bd6226b
josephwilk <joe@josephwilk.net>
2014-01-16 19:07:37 +0100

    helpers

--- init.el
+++ init.el
@@ -2,6 +2,8 @@
 
 (color-theme-gandalf)
 
+(live-use-dev-packs)
+
 (global-set-key (kbd "C-c <left>") 'windmove-left)
 (global-set-key (kbd "C-c <right>") 'windmove-right)
 (global-set-key (kbd "C-c <up>") 'windmove-up)
@@ -12,3 +14,18 @@
 (when (fboundp 'winner-mode) (winner-mode 0))
 
 (setq zone-programs [zone-pgm-drip])
+
+(defun music-layout () "Music split"
+  (interactive)
+  (progn
+    (split-window-horizontally)
+    (win-switch-right)
+    (minimize-window)
+    (win-switch-left)))
+
+(defun test-layout () "Code split"
+  (interactive)
+  (progn
+    (split-window-horizontally)
+    (win-switch-right)
+    (win-switch-left)))
