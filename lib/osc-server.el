1cb9df3bc8fa0837d577c7d4b4856a9d07ea90bc
Joseph Wilk <joe@josephwilk.net>
2015-03-18 22:13:28 +0100

    silliness

--- lib/osc-server.el
+++ lib/osc-server.el
@@ -2,16 +2,20 @@
 (require 'cl)
 
 (defvar osc-server nil "Connection to recieve msgs")
+(defvar flip-state t)
 
 (defun osc-connect ()
   (if (not osc-server)
       (setq osc-server
             (osc-make-server "localhost" 4558
                              (lambda (path &rest args)
-                               (print path)
+                               ;; (print path)
                                (cond
-                                ((string-match "/flip" path) (flip))
-                                ((string-match "/flop" path) (flop))))))))
+                                ((string-match "/beat" path)  (progn (if flip-state
+                                                                         (highlight)
+                                                                       (default))
+                                                                     (setq flip-state (not flip-state))))
+                                ((string-match "/crazy" path) (crazy))))))))
 
 (defun osc-make-server (host port default-handler)
   (make-network-process
@@ -36,28 +40,34 @@
 
 (provide 'osc-server)
 
-(defun flip ()
+(defun highlight ()
   (custom-set-faces
-   '(rainbow-delimiters-depth-1-face ((t (:foreground "blue"))))
-   '(rainbow-delimiters-depth-2-face ((t (:foreground "blue"))))
-   '(rainbow-delimiters-depth-3-face ((t (:foreground "#8b7500"))))
-   '(rainbow-delimiters-depth-4-face ((t (:foreground "#8b7500"))))
-   '(rainbow-delimiters-depth-5-face ((t (:foreground "#8b7500"))))
-   '(rainbow-delimiters-depth-6-face ((t (:foreground "#8b7500"))))
-   '(rainbow-delimiters-depth-7-face ((t (:foreground "#8b7500"))))
-   '(rainbow-delimiters-depth-8-face ((t (:foreground "#8b7500"))))
-   '(rainbow-delimiters-depth-9-face ((t (:foreground "#8b7500"))))
-   '(rainbow-delimiters-unmatched-face ((t (:foreground "red"))))))
+   '(rainbow-delimiters-depth-1-face ((t (:foreground "#545600"))))
+   '(rainbow-delimiters-depth-2-face ((t (:foreground "#FC5609"))))
+;;   '(window-number-face ((t (:foreground "#545611"))))
+   '(mode-line-buffer-id ((t (:foreground "#545611"))))
+   ))
 
-(defun flop ()
+;;(highlight)
+;;(default)
+
+(defun default ()
   (custom-set-faces
-   '(rainbow-delimiters-depth-1-face ((t (:foreground "red"))))
-   '(rainbow-delimiters-depth-2-face ((t (:foreground "red"))))
-   '(rainbow-delimiters-depth-3-face ((t (:foreground "#8e7560"))))
-   '(rainbow-delimiters-depth-4-face ((t (:foreground "#8b1540"))))
-   '(rainbow-delimiters-depth-5-face ((t (:foreground "#8b7350"))))
-   '(rainbow-delimiters-depth-6-face ((t (:foreground "#1f7540"))))
-   '(rainbow-delimiters-depth-7-face ((t (:foreground "#8b7300"))))
-   '(rainbow-delimiters-depth-8-face ((t (:foreground "#4b7200"))))
-   '(rainbow-delimiters-depth-9-face ((t (:foreground "#1b7400"))))
-   '(rainbow-delimiters-unmatched-face ((t (:foreground "red"))))))
+   '(rainbow-delimiters-depth-1-face ((t (:foreground "#FDDD0C"))))
+   '(rainbow-delimiters-depth-2-face ((t (:foreground "#FC5609"))))
+;;   '(window-number-face ((t (:foreground "#FDDD0C"))))
+   '(mode-line-buffer-id ((t (:foreground "#FDDD0C"))))
+   ))
+
+(defun crazy ()
+  (custom-set-faces
+   '(font-lock-builtin-face ((t (:foreground "#F34560"))))
+   '(font-lock-builtin-face ((t (:foreground "#000FF0"))))
+   `(mode-line ((t (:foreground "#011143" :background "#0000000"))))
+   '(mode-line-highlight ((t (:foreground "#F11111"))))
+   '(mode-line-emphasis ((t (:foreground "#F11111"))))
+   '(mode-line-buffer-id ((t (:foreground "#F11111"))))
+   '(custom-themed ((t :foreground "#F11111")))
+   '(vertical-border ((t :foreground "#101010" :background "#232232")))
+   ))
+;;(crazy)
